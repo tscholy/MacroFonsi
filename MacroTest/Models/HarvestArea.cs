e7a673ada091f6ccfc3f15b882eb7d690f98a389
tscholy <christina.tschol@students.fhv.at>
2018-05-22 22:04:09 +0200

    first commit

---
+++ MacroTest/Models/HarvestArea.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+
+namespace MacroTest.Models
+{
+    public  class HarvestArea
+    {
+        private Point leftPointStreetSide;
+        private Point rightPointStreetSide;
+
+        private Point leftPointWallSide;
+        private Point rightPointWallSide;
+
+        public HarvestArea()
+        {
+            
+        }
+
+        public Point LeftPointStreetSide { get => leftPointStreetSide; set => leftPointStreetSide = value; }
+        public Point RightPointStreetSide { get => rightPointStreetSide; set => rightPointStreetSide = value; }
+        public Point LeftPointWallSide { get => leftPointWallSide; set => leftPointWallSide = value; }
+        public Point RightPointWallSide { get => rightPointWallSide; set => rightPointWallSide = value; }
+    }
+}
